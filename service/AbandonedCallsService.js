27233e19ad2bab2f22c7ef4d5285a960bf41fdf5
Melissa Arellano <monserocha97@gmail.com>
2018-09-27 13:08:11 -0500

    Commit
    Actualizando nuevo proyecto con 4 servicios
    SIN implementacion **

--- service/AbandonedCallsService.js
+++ service/AbandonedCallsService.js
@@ -13,42 +13,31 @@
  * threshold Integer Number representing seconds (optional)
  * returns List
  **/
-var AbandonedCallsImplementation = require('../implementation/AbandonedCallsImplementation');
 exports.getAbandonedCalls = function(service,day,country,from,to,threshold) {
   return new Promise(function(resolve, reject) {
-
-    
-    AbandonedCallsImplementation.put(service,day,country,from,to,threshold).then((result)=>{
-      //console.log(result)
-
-      if (Object.keys(result).length > 0) {
-        resolve(result)
-      } else {
-        resolve();
-      }
-    }).catch((err) => {
-      reject(err);
-    });
-    })
-    /*
     var examples = {};
-    examples['application/json'] = [
-    {
-      "Service" : "ACARI",
-      "Day" : "2018-02-10",
-      "Country" : "Jamaica",
-      "CallsAbandoned" : 100,
-      "From" : "08:00",
-      "To" : "15:00",
-      "Threshold" : 45
-    } ];
-  */
-    
-    /*if (Object.keys(examples).length > 0) {
+    examples['application/json'] = [ {
+  "Service" : "ACARI",
+  "Day" : "2018-02-10",
+  "Country" : "Jamaica",
+  "CallsAbandoned" : 100,
+  "From" : "08:00",
+  "To" : "15:00",
+  "Threshold" : 45
+}, {
+  "Service" : "ACARI",
+  "Day" : "2018-02-10",
+  "Country" : "Jamaica",
+  "CallsAbandoned" : 100,
+  "From" : "08:00",
+  "To" : "15:00",
+  "Threshold" : 45
+} ];
+    if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
-    }*/
- 
+    }
+  });
 }
 
